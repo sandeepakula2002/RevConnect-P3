c8a0991f7e958164103cd4fc60b8b384cbc7748b
Vasanth-fr <vasanth2k19@gmail.com>
2026-03-12 12:36:33 +0530

    Updated Post test cases

---
+++ post-service/src/test/java/com/revconnect/post/PostServiceTest.java
@@ -0,0 +1,12 @@
+package com.revconnect.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PostServiceTest {
+   
+    @Test
+    void contextLoads(){
+    }
+}
